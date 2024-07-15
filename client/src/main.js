c796df4340dd68c6b043026e7b949061ca805894
Abe <anaberranstis@gmail.com>
2024-07-15 22:58:37 +0200

    deploy

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue';
 import io from 'socket.io-client';
 import router from './router';
 
-const socket = io('http://172.19.104.61:3000'); // Nahraďte YOUR_IP_ADDRESS skutočnou IP adresou vášho počítača
+// const socket = io('http://172.19.104.61:3000'); // Nahraďte YOUR_IP_ADDRESS skutočnou IP adresou vášho počítača
+const socket = io('https://mirkapoker-server.onrender.com'); // Nahraďte YOUR_IP_ADDRESS skutočnou IP adresou vášho počítača
 
 const app = createApp(App);
 app.config.globalProperties.$socket = socket;
