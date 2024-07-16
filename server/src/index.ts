9257f5b9e53767dcd6c96898db6edd55bbf3de95
Abe <anaberranstis@gmail.com>
2024-07-16 13:28:43 +0200

    s

--- server/src/index.ts
+++ server/src/index.ts
@@ -42,6 +42,7 @@ app.get('/room/:id', (req, res) => {
 
 // Catch-all handler to return index.html for any requests not handled above
 app.get('*', (req, res) => {
+  console.log(__dirname)
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
