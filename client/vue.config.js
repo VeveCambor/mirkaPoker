60f49abb083268c9ec38dcc730b06d2fa20f8131
Abe <anaberranstis@gmail.com>
2024-07-16 10:22:55 +0200

    s

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: '/',
+  publicPath: '/client',
   outputDir: 'dist',
   transpileDependencies: true,
   devServer: {
