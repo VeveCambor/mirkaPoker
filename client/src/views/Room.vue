238907f6d9529e01585e3d3a420365239b6da373
Abe <anaberranstis@gmail.com>
2024-07-17 10:30:43 +0200

    vercel deploy

--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -8,19 +8,34 @@
     </div>
     <div v-else>
       <p>Room {{ roomId }}</p>
-      <br/>
+      <br />
       <div v-if="isScrumMaster">
         <input v-model="storyTitle" placeholder="Enter story title" />
       </div>
       <div v-if="evaluationStarted">
         <h2>Evaluation in progress: {{ countdown }}</h2>
       </div>
-      <br/>
+      <br />
       <div class="players-container" v-if="room">
-        <div class="player" v-for="user in room.users" :key="user.name" :class="{ scrumMaster: user.isScrumMaster, voted: user.vote !== undefined }">
+        <div
+          class="player"
+          v-for="user in room.users"
+          :key="user.name"
+          :class="{
+            scrumMaster: user.isScrumMaster,
+            voted: user.vote !== undefined,
+          }"
+        >
           <p>{{ user.name }}</p>
-          <p v-if="room.votesRevealed && !user.isScrumMaster" class="vote">{{ user.vote }}</p>
-          <p v-else-if="user.name === userName && selectedCard !== null" class="vote">{{ selectedCard }}</p>
+          <p v-if="room.votesRevealed && !user.isScrumMaster" class="vote">
+            {{ user.vote }}
+          </p>
+          <p
+            v-else-if="user.name === userName && selectedCard !== null"
+            class="vote"
+          >
+            {{ selectedCard }}
+          </p>
         </div>
       </div>
       <p v-if="!isScrumMaster">Select your vote:</p>
@@ -29,19 +44,37 @@
           <button @click="castVote(card)">{{ card }}</button>
         </div>
       </div>
-      <br/>
+      <br />
       <div v-if="!room.votesRevealed">
-        <button v-if="isScrumMaster && !evaluationStarted" @click="startEvaluation">Start Evaluation</button>
-        <button v-if="isScrumMaster && !evaluationStarted" @click="revealVotes">Reveal Votes</button>
-        <button v-if="isScrumMaster && evaluationStarted" @click="revealVotes">Reveal Votes</button>
-        <button v-if="isScrumMaster && room.votesRevealed" @click="resetEvaluation">Restart Evaluation</button>
+        <button
+          v-if="isScrumMaster && !evaluationStarted"
+          @click="startEvaluation"
+        >
+          Start Evaluation
+        </button>
+        <button v-if="isScrumMaster && !evaluationStarted" @click="revealVotes">
+          Reveal Votes
+        </button>
+        <button v-if="isScrumMaster && evaluationStarted" @click="revealVotes">
+          Reveal Votes
+        </button>
+        <button
+          v-if="isScrumMaster && room.votesRevealed"
+          @click="resetEvaluation"
+        >
+          Restart Evaluation
+        </button>
       </div>
       <div v-if="room.votesRevealed">
         <h2>Votes</h2>
         <div class="results">
-          <p>Average vote: <b>{{ results.average }}</b></p>
+          <p>
+            Average vote: <b>{{ results.average }}</b>
+          </p>
         </div>
-        <button v-if="isScrumMaster" @click="resetEvaluation">Restart Evaluation</button>
+        <button v-if="isScrumMaster" @click="resetEvaluation">
+          Restart Evaluation
+        </button>
       </div>
       <div class="history" v-if="storyHistory.length > 0">
         <h2>Story History</h2>
@@ -62,30 +95,32 @@
       </div>
     </div>
     <div v-if="isScrumMaster && room" class="room-link">
-      <p>Room link: <a :href="roomLink">{{ roomLink }}</a></p>
+      <p>
+        Room link: <a :href="roomLink">{{ roomLink }}</a>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 export default {
-  name: 'RoomPage',
+  name: "RoomPage",
   data() {
     return {
       roomId: this.$route.params.id,
-      userName: '',
+      userName: "",
       room: null,
       joined: false,
       fibonacci: [1, 2, 3, 5, 8, 13, 21],
       results: {},
       evaluationStarted: false,
       countdown: 10,
-      errorMessage: '',
-      storyTitle: '',
+      errorMessage: "",
+      storyTitle: "",
       storyHistory: [],
-      selectedCard: null
+      selectedCard: null,
     };
   },
   computed: {
@@ -96,41 +131,46 @@ export default {
       return `${window.location.origin}/room/${this.roomId}`;
     },
     filteredUsers() {
-      return this.room ? this.room.users.filter(user => !user.isScrumMaster) : [];
-    }
+      return this.room
+        ? this.room.users.filter((user) => !user.isScrumMaster)
+        : [];
+    },
   },
   mounted() {
     // this.socket = io('http://localhost:3000');
-    this.socket = io('https://mirkapoker-server.onrender.com');
-    this.socket.on('updateRoom', (room) => {
+    this.socket = io("https://mirkapoker-server.onrender.com");
+    this.socket.on("updateRoom", (room) => {
       this.room = room;
     });
-    this.socket.on('results', (results) => {
+    this.socket.on("results", (results) => {
       this.results = results;
       if (this.isScrumMaster && this.storyTitle) {
-        this.storyHistory.push({ title: this.storyTitle, average: results.average });
-        this.storyTitle = '';
+        this.storyHistory.push({
+          title: this.storyTitle,
+          average: results.average,
+        });
+        this.storyTitle = "";
       }
     });
-    this.socket.on('startEvaluation', () => {
+    this.socket.on("startEvaluation", () => {
       this.evaluationStarted = true;
       this.countdown = 10;
       this.selectedCard = null; // Reset selected card on evaluation start
     });
-    this.socket.on('countdown', (count) => {
+    this.socket.on("countdown", (count) => {
       this.countdown = count;
       if (count === 0) {
         this.evaluationStarted = false;
       }
     });
-    this.socket.on('resetCard', () => {
+    this.socket.on("resetCard", () => {
       this.selectedCard = null;
     });
-    this.socket.on('roomClosed', () => {
-      alert('Room has been closed.');
+    this.socket.on("roomClosed", () => {
+      alert("Room has been closed.");
       this.joined = false;
       this.room = null;
-      localStorage.removeItem('savedRoom');
+      localStorage.removeItem("savedRoom");
     });
 
     const userName = this.$route.query.userName;
@@ -141,46 +181,57 @@ export default {
   },
   methods: {
     joinRoom() {
-      this.socket.emit('joinRoom', { roomId: this.roomId, userName: this.userName }, (response) => {
-        if (response.success) {
-          this.joined = true;
-          this.errorMessage = '';
-          const savedRoom = {
-            id: this.roomId,
-            userName: this.userName,
-            isScrumMaster: this.isScrumMaster
-          };
-          localStorage.setItem('savedRoom', JSON.stringify(savedRoom));
-        } else {
-          this.errorMessage = response.message;
+      this.socket.emit(
+        "joinRoom",
+        { roomId: this.roomId, userName: this.userName },
+        (response) => {
+          if (response.success) {
+            this.joined = true;
+            this.errorMessage = "";
+            const savedRoom = {
+              id: this.roomId,
+              userName: this.userName,
+              isScrumMaster: this.isScrumMaster,
+            };
+            localStorage.setItem("savedRoom", JSON.stringify(savedRoom));
+            this.$router.push({
+              path: `/room/${this.roomId}`,
+              query: { userName: this.userName },
+            });
+          } else {
+            this.errorMessage = response.message;
+          }
         }
-        this.$router.push({
-          path: `/room/${this.roomId}`,
-          query: { userName: this.userName }
-      });
-      });
+      );
     },
     castVote(card) {
       this.selectedCard = card;
-      this.socket.emit('vote', { roomId: this.roomId, userName: this.userName, vote: card });
+      this.socket.emit("vote", {
+        roomId: this.roomId,
+        userName: this.userName,
+        vote: card,
+      });
     },
     revealVotes() {
-      this.socket.emit('revealVotes', { roomId: this.roomId, userName: this.userName });
+      this.socket.emit("revealVotes", {
+        roomId: this.roomId,
+        userName: this.userName,
+      });
     },
     startEvaluation() {
-      this.socket.emit('startEvaluation', this.roomId);
+      this.socket.emit("startEvaluation", this.roomId);
     },
     resetEvaluation() {
       this.evaluationStarted = false;
       this.countdown = 10;
       // this.selectedCard = null;
-      this.socket.emit('resetEvaluation', this.roomId);
+      this.socket.emit("resetEvaluation", this.roomId);
       // this.socket.emit('resetCard', this.roomId); // Emit reset card event
-    }
+    },
   },
   beforeUnmount() {
-    localStorage.removeItem('savedRoom');
-  }
+    localStorage.removeItem("savedRoom");
+  },
 };
 </script>
 
